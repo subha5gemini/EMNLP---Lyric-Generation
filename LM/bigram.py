c542c1d74a94764f64911629d5d412bc00244bc7
842974287 <842974287@qq.com>
2019-04-21 17:32:03 -0400

    three different clean methods

--- LM/bigram.py
+++ LM/bigram.py
@@ -1,5 +1,4 @@
 import pickle
-import pandas as pd
 import numpy as np
 from sklearn.preprocessing import normalize
 import sys
--- generateData.py
+++ generateData.py
@@ -3,19 +3,14 @@ import string
 import enchant
 import sys
 from pattern.en import lemma
-sys.path.append('../lib')
+sys.path.append('./lib')
 
 from show_process import ShowProcess
-
-lyric_csv = './data/lyrics.csv'
-lyric_data = './data/lyrics.data'
-lyric_nd = './data/lyrics.nd'
-lyric_pattern = './data/lyric.pattern'
 
 d_GB = enchant.Dict("en_GB")
 d_US = enchant.Dict("en_US")
 
-def clean(tokens):
+def clean_data(tokens):
     clean = []
 
     for token in tokens:
@@ -26,7 +21,7 @@ def clean(tokens):
         if token != '' and (d_GB.check(token) or d_US.check(token)):
             clean.append(token)
 
-    return clean.join(' ') + '\n'
+    return ' '.join(clean) + '\n'
 
 def hasNumbers(inputString):
     return any(char.isdigit() for char in inputString)
@@ -42,7 +37,7 @@ def clean_no_digit(tokens):
         if token != '' and (d_GB.check(token) or d_US.check(token)) and not hasNumbers(token):
             clean.append(token)
 
-    return clean.join(' ') + '\n'
+    return ' '.join(clean) + '\n'
 
 def clean_pattern(tokens):
     clean = []
@@ -56,32 +51,33 @@ def clean_pattern(tokens):
             token = lemma(token)
             clean.append(token)
 
-    return clean.join(' ') + '\n'
+    return ' '.join(clean) + '\n'
 
-#read the csv file and store the lyrics in lyrics_store []
-csvReader = pd.read_csv(lyric_data)
-csvReader = csvReader.drop(0)
-lyrics_store = csvReader['lyrics']
+if __name__ == '__main__':
+    lyric_csv = './data/lyrics.csv'
+    lyric_data = './data/lyrics.' + sys.argv[1]
 
-#get the tokens in lyrics
-data = ''
-nd = ''
-pattern = ''
-process = ShowProcess(len(lyrics_store))
+    #read the csv file and store the lyrics in lyrics_store []
+    csvReader = pd.read_csv(lyric_csv)
+    csvReader = csvReader.drop(0)
+    lyrics_store = csvReader['lyrics']
 
-for line in lyrics_store:
-    process.show_process()
-    line = str(line)
-    tokens = line.split()
-    data += clean(tokens)
-    nd += clean_no_digit(tokens)
-    pattern += clean_pattern(tokens)
+    #get the tokens in lyrics
+    data = ''
+    process = ShowProcess(len(lyrics_store))
 
-with open(lyric_data, 'w+') as f:
-    f.wirte(data)
+    if sys.argv[1] == 'data':
+        clean = clean_data
+    elif sys.argv[1] == 'nd':
+        clean = clean_nd
+    else:
+        clean = clean_pattern
 
-with open(lyric_nd, 'w+') as f:
-    f.wirte(nd)
+    for line in lyrics_store:
+        process.show_process()
+        line = str(line)
+        tokens = line.split()
+        data += clean(tokens)
 
-with open(lyric_pattern, 'w+') as f:
-    f.wirte(pattern)
+    with open(lyric_data, 'w+') as f:
+        f.wirte(data)
