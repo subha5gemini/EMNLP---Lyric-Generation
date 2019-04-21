bcfea97508d30d9dd84118fd5dc75a0b68c02518
842974287 <842974287@qq.com>
2019-04-20 23:57:46 -0400

    progress showing tool

--- LM/bigram.py
+++ LM/bigram.py
@@ -1,8 +1,8 @@
 import pandas as pd
 import numpy as np
-from collections import OrderedDict, Counter
 from sklearn.preprocessing import normalize
 from word_index_dict import clean
+import sys
 
 lyric_data = '../data/lyrics.csv'
 word_dict_file = '../data/word_index_dict'
@@ -10,32 +10,58 @@ bigram_prob = '../data/bigram_prob'
 START = '<s>'
 END = '</s>'
 
-class OrderedCounter(Counter, OrderedDict):
-    pass
+def train():
+    #read the csv file and store the lyrics in lyrics_store []
+    csvReader = pd.read_csv(lyric_data)
+    csvReader = csvReader.drop(0)
+    lyrics_store = csvReader['lyrics']
+    
+    #get the tokens in lyrics
+    with open(word_dict_file, 'r') as f:
+        word_index_dict = eval(f.read())
+    
+    counts = np.zeros((len(word_index_dict), len(word_index_dict)), dtype = float)
+    
+    for line in lyrics_store:
+        line = str(line)
+        tokens = clean(line.split())
+        previous = END
+    
+        for token in reversed(tokens):
+            counts[word_index_dict[previous]][word_index_dict[token]] += 1
+            previous = token
+    
+        counts[word_index_dict[previous]][word_index_dict[START]] += 1
+    
+    probs = normalize(counts, norm='l1')
+    
+#    with open(bigram_prob, 'w+') as f:
+#        f.write(str(probs))
+        
+    return word_index_dict, probs
 
-#read the csv file and store the lyrics in lyrics_store []
-csvReader = pd.read_csv(lyric_data)
-csvReader = csvReader.drop(0)
-lyrics_store = csvReader['lyrics']
+def generate(pair, word_index_dict, probs):
+    index_word_dict = {v: k for k, v in word_index_dict.items()}
+    sentence = []
+    
+    for i in range(2):
+        returnSTR = pair[i]
+        prevWord = pair[i]
+        num_words = 1
+        
+        while(True):
+            wordIndex = np.random.choice(len(word_index_dict), 1, p = list(probs[word_index_dict[prevWord]]))
+            word = index_word_dict[wordIndex[0]]
+            returnSTR = word + ' ' + returnSTR
+            num_words +=1
+            prevWord = word
+            
+            if word == "<s>" or num_words == 12:
+                break
+            
+        sentence.append(returnSTR)
+        
+    return sentence
 
-#get the tokens in lyrics
-with open(word_dict_file, 'r') as f:
-    word_index_dict = eval(f.read())
-
-counts = np.zeros((len(word_index_dict), len(word_index_dict)), dtype = float)
-
-for line in lyrics_store:
-    line = str(line)
-    tokens = clean(line.split())
-    previous = END
-
-    for token in reversed(tokens):
-        counts[word_index_dict[previous]][word_index_dict[token]] += 1
-        previous = token
-
-    counts[word_index_dict[previous]][word_index_dict[START]] += 1
-
-probs = normalize(counts, norm='l1')
-
-with open(bigram_prob, 'w+') as f:
-    f.write(str(probs))
+word_index_dict, probs = train()
+print(generate(('shop', 'top'), word_index_dict, probs))
--- LM/word_index_dict.py
+++ LM/word_index_dict.py
@@ -1,9 +1,18 @@
 import pandas as pd
+import string
+import enchant
+import sys
+sys.path.append("../")
+
+from show_process import ShowProcess
 
 lyric_data = '../data/lyrics.csv'
 word_dict_file = '../data/word_index_dict'
 START = '<s>'
 END = '</s>'
+
+d_GB = enchant.Dict("en_GB")
+d_US = enchant.Dict("en_US")
 
 def clean(tokens):
     clean = []
@@ -11,8 +20,10 @@ def clean(tokens):
     for token in tokens:
         #normalize token
         token = token.lower()
-        clean.append(token)
-        #drop stop words
+        token.strip(string.punctuation)
+
+        if d_GB.check(token) or d_US.check(token):
+            clean.append(token)
 
     return clean
 
@@ -24,8 +35,10 @@ lyrics_store = csvReader['lyrics']
 #get the tokens in lyrics
 lexicon = set()
 word_index_dict = {}
+process = ShowProcess(len(lyrics_store))
 
 for line in lyrics_store:
+    process.show_process()
     line = str(line)
     lexicon.update(set(clean(line.split())))
 
